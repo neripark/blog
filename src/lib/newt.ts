657bb6d703bd5f5a6a10523cff2bef5b9b366bec
neripark <neripark@gmail.com>
2023-02-08 18:01:52 +0900

    init newt client

---
+++ src/lib/newt.ts
@@ -0,0 +1,20 @@
+// ref: https://www.newt.so/docs/tutorials/get-contents-in-nextjs
+import type { Post } from "@/types/Post";
+import { createClient } from "newt-client-js";
+
+const client = createClient({
+  spaceUid: process.env.NEWT_SPACE_UID + "",
+  token: process.env.NEWT_CDN_API_TOKEN + "",
+  apiType: "cdn",
+});
+
+export const getPosts = async () => {
+  const { items } = await client.getContents<Post>({
+    appUid: "blog2",
+    modelUid: "post",
+    query: {
+      select: ["_id", "title", "slug", "body"],
+    },
+  });
+  return items;
+};
---
+++ src/types/Post.ts
@@ -0,0 +1,7 @@
+// newt の型
+export interface Post {
+  _id: string;
+  title: string;
+  slug: string;
+  body: string;
+}
